f31b0c27ed26acbe7d3ba46c169c347621fd0b8c
calebmutisya <mutisyacalebmusau@gmail.com>
2023-12-01 19:24:17 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr.Chocolate', 9, 'M', 'Chocolate', 'Jovial', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 5, 'M', 'Brown', 'Playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 7, 'F', 'Black', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 8, 'M', 'Brown', 'Disciplined', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 6, 'F', 'Honey', 'Sweet', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 3, 'M', 'Green', 'Mischievous', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 4, 'F', 'White', 'Friendly', 1);
+
+-- Inserting a record with an unnamed bear
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES (2, 'F', 'Gray', 'Shy', 1);
